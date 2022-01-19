d176ac25aa8a8841f1b9b664aa3d72323d6dbf5f
juring <zlxlwnfl@naver.com>
2022-01-19 21:30:28 +0900

    #5 기본적인 샘플 컴포넌트들 추가

--- src/views/GamePage/GamePage.tsx
+++ src/views/GamePage/GamePage.tsx
@@ -4,6 +4,9 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '../../components/Typography/Typography';
 import Header from '../../components/Header/Header';
+import CustomizedTables from '../../components/Table/CustomizedTables';
+import CustomButton from '../../components/Button/CustomButton';
+import OutlinedCard from '../../components/Card/OutlinedCard'
 
 const GamePage = () => {
     return (
@@ -15,17 +18,31 @@ const GamePage = () => {
                         Game
                     </Typography>
                 </Box>
-                <Box sx={{ ml: 20, mr: 20 }}>
+                <Box sx={{ ml: 10, mr: 10 }}>
                     <Grid container spacing={2}>
                         <Grid item xs={8}>
-                            랭킹 리스트
+                            <Typography variant="h4" gutterBottom marked="left" align="left">
+                                RANK
+                            </Typography>
+                            <CustomizedTables />
                         </Grid>
-                        <Grid item xs={4}>
-                            <Grid item xs={4}>
-                                게임 시작 버튼
+                        <Grid item xs>
+                            <Grid item xs>
+                            <CustomButton
+                                sx={{ mt: 3, mb: 2 }}
+                                type="submit"
+                                variant="contained"
+                                size="large"
+                                color="secondary"
+                                fullWidth
+                            >
+                                Game Start
+                            </CustomButton>
                             </Grid>
-                            <Grid item xs={4}>
-                                관련 포스트들
+                            <Grid item xs>
+                                <OutlinedCard />
+                                <OutlinedCard />
+                                <OutlinedCard />
                             </Grid>
                         </Grid>
                     </Grid>
