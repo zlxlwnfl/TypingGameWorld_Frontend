09bef2a439b57479215c51f06143b284e03d2bd3
juring <zlxlwnfl@naver.com>
2022-01-19 21:06:13 +0900

    #5 대략적인 레이아웃 셋팅

--- src/views/GamePage/GamePage.tsx
+++ src/views/GamePage/GamePage.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Typography from '../../components/Typography/Typography';
 import Header from '../../components/Header/Header';
 
@@ -13,6 +14,21 @@ const GamePage = () => {
                     <Typography variant="h3" gutterBottom marked="center" align="center">
                         Game
                     </Typography>
+                </Box>
+                <Box sx={{ ml: 20, mr: 20 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={8}>
+                            랭킹 리스트
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Grid item xs={4}>
+                                게임 시작 버튼
+                            </Grid>
+                            <Grid item xs={4}>
+                                관련 포스트들
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Box>
             </Container>
         </React.Fragment>
